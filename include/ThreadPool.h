5fb3055e5bac399ff826d1b62178acfde150e1ae
Flysky701 <18458871liu@gmail.com>
2025-02-02 23:02:46 +0800

    基础class

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -1,1 +1,40 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+// #include <bits/stdc++.h> 
+#include <vector>
+#include <queue>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <functional>
+#include <stdexcept>
+
+
+
+class ThreadPool{
+    public:
+        ThreadPool (size_t);
+        template<class F, class... Args> 
+        auto ThreadPool::enqueue(F &&f,Args&&... args )
+            -> std::future<typename std::result_of<F(Args...)> :: type>;
+        ~ThreadPool();
+    
+    private:
+        std::vector< std::thread > workers;
+
+        std::mutex mutex_queue;
+        
+
+    
+    
+
+
+
+}
+
+
+#endif
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,10 @@
+#include <thread>
+#include <mutex>
+#include <vector>
 
+int main() {
+    std::vector<std::thread> workers;
+    std::mutex mutex_queue;
+    return 0;
+}
 // This is a test 
