be36e6a5932beb699b5df6b31d946e81c70f45f5
Flysky701 <18458871liu@gmail.com>
2025-02-03 20:32:10 +0800

    测试完成

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,40 @@
 #include <vector>
 #include <future>
 
-int main() {
-   
+int main()
+{
+
     auto get = []()
     {
-        for(int i = 1; i <= 10; i ++)
+        for (int i = 1; i <= 10; i++)
             std::cout << i << " ";
         std::cout << std::endl;
     };
-   
+
     ThreadPool abc(4);
-    std::vector< std::future<void>> res;
-    res.emplace_back(abc.enqueue(get));
+    std::vector<std::future<void>> res;
+    for (int i = 1; i <= 10; i++)
+    {
+        res.emplace_back(abc.enqueue(get));
+    }
 
+    ThreadPool pool(4);
+    std::vector<std::future<int>> results;
+    for (int i = 0; i < 8; ++i)
+    {
+        results.emplace_back(
+            pool.enqueue([i]
+                         {
+                  std::cout << "hello " << i << std::endl;
+                  std::this_thread::sleep_for(std::chrono::seconds(1));
+                  std::cout << "world " << i << std::endl;
+                  return i*i; }));
+    }
+    for (auto &&result : results)
+        std::cout << result.get() << ' ';
+    std::cout << std::endl;
 
+    int a;
+    std::cin >> a;
 }
-// This is a test 
+// This is a test
